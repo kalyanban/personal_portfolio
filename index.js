a5d281e62d815dfb7796e15aef3fb7c58c4ef452
kalyanban <bandarukalyangoud430@gmail.com>
2025-01-08 15:20:11 +0530

    personal_portfolio

--- index.js
+++
@@ -1,6 +0,0 @@
-const menuButton = document.getElementById("menuButton")
-const mobileNavList = document.querySelector(".mobile-nav-tabs-container")
-
-menuButton.addEventListener("click", () => {
-    mobileNavList.classList.toggle("show")
-})
