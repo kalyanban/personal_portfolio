f66191b4697460be4fd2d64c2f2fec2d51d0c897
kalyanban <bandarukalyangoud430@gmail.com>
2025-01-27 16:49:52 +0530

    personal_portfolio

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const mobileNavList = document.querySelector(".mobile-nav-tabs-container")
 
 const portfolioLinkButton = document.getElementById("portfolioLinkButton")
 const nxtwatchLinkButton = document.getElementById("nxtwatchLinkButton")
+const moviesAppLinkButton = document.getElementById("moviesAppLinkButton")
 
 menuButton.addEventListener("click", () => {
     mobileNavList.classList.toggle("show")
@@ -15,5 +16,10 @@ portfolioLinkButton.addEventListener("click", () =>{
 
 nxtwatchLinkButton.addEventListener("click", () => {
     const appUrl = "https://nxtwatchkb.ccbp.tech/"
+    window.open(appUrl, "_blank")
+})
+
+moviesAppLinkButton.addEventListener("click", () => {
+    const appUrl = "https://moviesappkb.ccbp.tech"
     window.open(appUrl, "_blank")
 })
